33e84ab379d1ed7a554e6f97652530288167d915
danielsf <scott.f.daniel@gmail.com>
2015-01-14 16:08:29 -0800

    removed unneeded print statements

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,12 +1,2 @@
 NJOBS=1
 export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true PREFIX="$PREFIX"/lib"
-
-build()
-{
-    echo "SFD build"
-    echo $PRODUCT_DIR
-    echo $LD_LIBRARY_PATH
-    echo $PATH
-    echo $C_INCLUDE_PATH
-    default_build
-}
